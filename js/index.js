056abfd6d911dd6daac0f52c99601cc2275755a3
Ahmed Nady <120312236+NAADDY@users.noreply.github.com>
2024-02-15 00:51:49 +0200

    responsive edit and some other edits

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 let container = document.getElementById("container");
 let mobile_header = document.getElementById("mobile_header");
+let complete = document.getElementById("complete");
+let btt = document.getElementById("btt");
+let showless = document.getElementById("showless");
 // let cancell = document.getElementById("cancell");
 
 function cancell(){
@@ -10,4 +13,15 @@ function doo(){
     container.style.display = "none";
     mobile_header.style.display="flex";
 
+}
+function showw(){
+    complete.style.display="inline-block";
+    btt.style.display="none";
+    showless.style.display="block";
+
+}
+function showless_hide(){
+    complete.style.display="none";
+    btt.style.display="block";
+    showless.style.display="none";  
 }
